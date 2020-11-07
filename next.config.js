b6ce75d28fca4d1d4da6f3157521ca73b9f85b07
Yusuke Hayashi <yusuke8h@gmail.com>
2020-11-08 00:31:35 +0900

    feat: add i18n next config

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const nextConfig = {
+  i18n: {
+    locales: ['ja', 'en'],
+    defaultLocale: 'ja',
+  },
+  domains: [
+    {
+      domain: 'webv.jp',
+      defaultLocale: 'ja',
+    },
+    {
+      domain: 'en.webv.jp',
+      defaultLocale: 'en',
+    },
+  ],
+}
+
+module.exports = nextConfig
